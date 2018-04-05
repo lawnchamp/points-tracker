87583337442d5de6fd2f01715ded6d83bfd31ba8
Riley Shenk <rileyshenk@learnzillion.com>
2018-04-05 16:26:39 -0400

    added firebase credentials

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// firebase.initializeApp({
+//   apiKey: 'AIzaSyDJGzhHLQp8oW794Egqm7j1UToE4CmHpPk',
+//   authDomain: 'point-tracker18.firebaseapp.com',
+//   databaseURL: 'https://point-tracker18.firebaseio.com',
+//   projectId: 'point-tracker18',
+//   storageBucket: 'point-tracker18.appspot.com',
+//   messagingSenderId: '727305853236'
+// })
 
 new Vue({
   router,
