4c0e27dfbf3a0dc495c1d5dc9a39edf4e7886ee5
Riley Shenk <rileyshenk@learnzillion.com>
2018-04-05 16:28:03 -0400

    added more seed data

--- src/data/seedCompetitions.js
+++ src/data/seedCompetitions.js
@@ -3,15 +3,29 @@ export default [
     name: 'dodge ball',
     winningTeam: 'black',
     pointsAwarded: 10
-  },
-  {
+  }, {
     name: 'kickball',
     winningTeam: 'blue',
     pointsAwarded: 20
-  },
-  {
+  }, {
     name: 'capture the flag',
     winningTeam: 'blue',
     pointsAwarded: 5
+  }, {
+    name: 'soccer',
+    winningTeam: 'red',
+    pointsAwarded: 15
+  }, {
+    name: 'dodge ball',
+    winningTeam: 'purple',
+    pointsAwarded: 12
+  }, {
+    name: 'long jump',
+    winningTeam: 'brown',
+    pointsAwarded: 8
+  }, {
+    name: 'basketball',
+    winningTeam: 'orange',
+    pointsAwarded: 17
   }
 ]
