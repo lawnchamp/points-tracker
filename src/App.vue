3079e2479bca839c6534e15e1fde75dd4262c950
Riley Shenk <rileyshenk@learnzillion.com>
2018-05-27 15:59:41 -0400

    home and about pages are not needed

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="app" class="bg-grey-lightest">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
 import './styles/app.scss'
+
+export default {
+  name: 'app'
+}
 </script>
 
 <style lang="scss">
@@ -19,15 +19,5 @@ import './styles/app.scss'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
 
 Vue.use(Router)
 
@@ -9,13 +7,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      component: About
     }
   ]
 })
