deeb6f05bab3d550e2b488589fe612803a3107db
Riley Shenk <rileyshenk@learnzillion.com>
2018-06-01 14:27:52 -0400

    using vuex in weights

--- src/views/Weights.vue
+++ src/views/Weights.vue
@@ -7,52 +7,45 @@
           {{formattedName(name)}}
         </td>
         <td>
-          <input type="number" v-model="weights[name]" class="rounded-lg py-3 px-2"/>
+          <input type="number" v-model.number="points.value" class="rounded-lg py-3 px-2"/>
         </td>
 
         <td>
           <button @click.prevent="remove(name)">X</button>
         </td>
       </tr>
-      <!-- <tr> -->
-        <NewWeight @new-weight="addNewWeight"></NewWeight>
-      <!-- </tr> -->
+      <NewWeight @new-weight="addNewWeight"></NewWeight>
     </table>
-    <inc></inc>
   </div>
 </template>
 
 <script>
 
-import competitionWeights from '@/data/competitionWeights.js'
 import AuthenticateButton from '@/components/AuthenticateButton.vue'
-import inc from '@/components/inc.vue'
 import NewWeight from '@/components/NewWeight.vue'
-import Vue from 'vue'
 import {startCase as _startCase} from 'lodash'
 
 export default {
   name: 'Weights',
   components: {
     AuthenticateButton,
-    NewWeight,
-    inc
+    NewWeight
   },
-  data () {
-    return {
-      weights: competitionWeights
+  computed: {
+    weights () {
+      return this.$store.state.weights
     }
   },
   methods: {
     remove (name) {
-      Vue.delete(this.weights, name)
+      this.$store.dispatch('removeWeight', name)
     },
-    addNewWeight ({name, value}) {
-      Vue.set(this.weights, name, value)
+    addNewWeight (newWeight) {
+      this.$store.dispatch('addWeight', newWeight)
     },
     formattedName (name) {
       return _startCase(name)
-    },
+    }
   }
 }
 </script>
