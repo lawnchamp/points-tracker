82c571de28410aa5c0cbdf01e17053daab1b33d3
Riley Shenk <rileyshenk@learnzillion.com>
2018-07-15 22:57:04 -0400

    wip - authenticate button redesign
    picture is hard coded and there needs to be a dropdown

--- src/components/AuthenticateButton.vue
+++ src/components/AuthenticateButton.vue
@@ -1,23 +1,13 @@
 <template>
-  <div class="static">
-    <div class="absolute pin-t pin-r py-2 px-4">
-      <!-- <div v-if="currentUser">{{currentUser.email}}</div> -->
-      <button class="uppercase text-sm hover:text-blue-dark text-blue-darker" @click="changeAuthState">
-        {{loginOrOut}}
-      </button>
-    </div>
-  </div>
+  <button type="button" @click="changeAuthState">
+    <img class="flex-no-shrink block h-10 w-10 border-2 border-grey-darker hover:border-white rounded-full" src="https://avatars0.githubusercontent.com/u/13643618?v=4" alt="">
+  </button>
 </template>
 
 <script>
 
 export default {
   name: 'AuthenticateButton',
-  computed: {
-    loginOrOut () {
-      return this.$store.getters.authenticatedUser ? 'logout' : 'sign in'
-    }
-  },
   methods: {
     changeAuthState () {
       if (this.$store.getters.authenticatedUser) {
