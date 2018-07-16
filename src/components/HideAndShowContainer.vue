fe041367a2bc8700a2844cef7012349f6012bb64
Riley Shenk <rileyshenk@learnzillion.com>
2018-07-16 00:16:12 -0400

    starting site redesign

---
+++ src/components/HideAndShowContainer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container mx-auto max-w-md px-4 py-1">
+    <div class="bg-white rounded-lg shadow p-2">
+      <div @click="show = !show">
+        <div class="font-semibold inline capitalize text-2xl">
+          <slot name="title"></slot>
+        </div>
+      </div>
+      <div v-show="show" class="border-t">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    initialShow: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  data () {
+    return {
+      show: this.initialShow
+    }
+  }
+}
+</script>
