bc77a01504f0943ae5163293929075b89a815715
Riley Shenk <rileyshenk@learnzillion.com>
2018-07-18 22:55:05 -0400

    pulled out site header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app" class="bg-grey-lighter">
+    <SiteHeader/>
     <router-view/>
   </div>
 </template>
 
 <script>
 import './styles/app.scss'
+import SiteHeader from '@/components/SiteHeader.vue'
 
 export default {
   name: 'app',
+  components: { SiteHeader },
   created () {
     this.$store.dispatch('getCompetitions')
     this.$store.dispatch('getWeights')
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="bg-black antialiased pb-32">
+    <div class="container mx-auto px-4">
+      <div class="flex justify-between items-center py-4 border-b border-grey-darkest">
+        <div class="flex items-center">
+          <svg class="text-grey-lighter h-8 w-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M1 10h3v10H1V10zM6 0h3v20H6V0zm5 8h3v12h-3V8zm5-4h3v16h-3V4z"/></svg>
+          <div class= "font-semibold text-2xl text-grey-darker antialiased px-4 pt-3">
+            {{ campTitle }}
+          </div>
+        </div>
+        <div>
+          <AuthenticateButton/>
+        </div>
+      </div>
+      <nav class="flex py-2" v-if="isAdmin">
+        <router-link class="text-grey-darker no-underline mr-2 px-2 py-2 rounded hover:bg-black-20 hover:text-white" to="/points">Points</router-link>
+        <router-link class="text-grey-darker no-underline mr-2 px-2 py-2 rounded hover:bg-black-20 hover:text-white" to="/weights">Weights</router-link>
+        <router-link class="text-grey-darker no-underline mr-2 px-2 py-2 rounded hover:bg-black-20 hover:text-white" to="/Teams">Teams</router-link>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+import AuthenticateButton from '@/components/AuthenticateButton.vue'
+export default {
+  components: { AuthenticateButton },
+  data () {
+    return {
+      campTitle: 'YouthCamp 2018'
+    }
+  },
+  computed: {
+    isAdmin () {
+      return this.$store.getters.isAdmin
+    }
+  }
+}
+</script>
