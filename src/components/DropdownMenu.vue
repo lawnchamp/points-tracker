6f41b0632cdb4b1439c9b3fbca1f42b1dd3d5c0e
Riley Shenk <rileyshenk@learnzillion.com>
2018-07-22 00:28:11 -0400

    dropdown now handles login and out
    however this is very much v1. dropdown should close when clicking
    outside dropdown

--- src/components/DropdownMenu.vue
+++ src/components/DropdownMenu.vue
@@ -4,15 +4,14 @@
       <img v-if="isAuthenticated" class="flex-no-shrink block h-10 w-10 border-2 border-grey-darker hover:border-white rounded-full" src="https://avatars0.githubusercontent.com/u/13643618?v=4" alt="">
       <svg v-else class="text-grey-dark flex-no-shrink block h-10 w-10 border-2 border-grey-darker hover:border-white rounded-full" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zM7 6v2a3 3 0 1 0 6 0V6a3 3 0 1 0-6 0zm-3.65 8.44a8 8 0 0 0 13.3 0 15.94 15.94 0 0 0-13.3 0z"/></svg>
     </button>
-    <div v-show="show" class="absolute pin-r w-16 bg-grey-lightest rounded z10 mask box-shadow">
-      <div class="px-4 py-2 border rounded-t no-select">
+    <div v-show="show" class="absolute pin-r w-64 bg-grey-lightest rounded z10 mask box-shadow">
+      <div v-if="userEmail" class="px-4 py-4 border rounded-t no-select bg-grey-light">
         <h4 class="text-dark-softer flex-spaced flex-y-center">
-          User name and emil
+          {{ userEmail }}
         </h4>
       </div>
-      <div class="px-4 py-3 no-select border-r border-b border-l rounded-b">
-        <h4 class="mb-2">Logout</h4>
-        <p class="text-sm">some subtext</p>
+      <div @click="changeAuthState" class="clickable px-4 py-4 no-select border-r border-b border-l rounded-b hover:bg-grey-light">
+        <h4>{{ logInOrOut }}</h4>
       </div>
     </div>
     <!-- <AuthenticateButton/> -->
@@ -27,6 +26,27 @@ export default {
   data () {
     return {
       show: false
+    }
+  },
+  computed: {
+    isAuthenticated () {
+      return !!this.$store.getters.authenticatedUser
+    },
+    userEmail () {
+      return this.$store.getters.authenticatedUser
+    },
+    logInOrOut () {
+      return this.isAuthenticated ? 'Logout' : 'Login'
+    }
+  },
+  methods: {
+    changeAuthState () {
+      if (this.$store.getters.authenticatedUser) {
+        this.$store.dispatch('userSignOut')
+      } else {
+        this.$router.replace('login')
+      }
+      this.show = !this.show
     }
   }
 }
