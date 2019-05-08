b9e95f2f60c1fcefc7d6af447879641ff5dd4248
Riley Shenk <rileyshenk@learnzillion.com>
2019-05-08 14:16:37 -0400

    temporarily disabling sorting ability

--- src/components/PointsContainer.vue
+++ src/components/PointsContainer.vue
@@ -32,7 +32,7 @@
         <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
       </div>
     </div> -->
-    <select
+    <!-- <select
       name="sort by"
       class="h-6 block appearance-none w-full bg-white border border-grey-light hover:border-grey px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
       @change="selectedTeamSort = $event.target.value"
@@ -40,7 +40,7 @@
       <option v-for="team in teamNames" :value="team" :key="team" class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker">
         {{ team }}
       </option>
-    </select>
+    </select> -->
     <CompetitionRow
       v-for="competition in orderedViewableCompetitions"
       v-bind="competition"
