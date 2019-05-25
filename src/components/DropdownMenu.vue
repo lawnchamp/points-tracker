bd2327e60d58ba78f1405729632c5f9dd45df96e
Riley Shenk <rjshenk@gmail.com>
2019-05-25 15:05:39 -0400

    bringing dropdown modal to front of page

--- src/components/DropdownMenu.vue
+++ src/components/DropdownMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative">
+  <div class="relative z-50">
     <button type="button" @click.prevent="show = !show">
       <img v-if="photoURL" :src="photoURL" alt="" class="text-grey-dark flex-no-shrink block h-10 w-10 border-2 border-grey-darker hover:border-white rounded-full">
       <svg v-else-if="isAuthenticated" class="text-grey-dark flex-no-shrink block h-10 w-10 border-2 border-grey-darker hover:border-white rounded-full" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zM7 6v2a3 3 0 1 0 6 0V6a3 3 0 1 0-6 0zm-3.65 8.44a8 8 0 0 0 13.3 0 15.94 15.94 0 0 0-13.3 0z"/></svg>
