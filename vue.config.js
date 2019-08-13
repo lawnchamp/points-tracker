625ab8ba37d90eb938aba52314eb3a294ca16006
Riley Shenk <rjshenk@gmail.com>
2019-08-12 21:58:57 -0400

    webpack test

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
+const RollbarSourceMapPlugin = require('rollbar-sourcemap-webpack-plugin')
+
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new RollbarSourceMapPlugin({
+        accessToken: 'd71e337e9631468e8564e79178e7883f',
+        version: 'version-1',
+        publicPath: 'https://points.live/'
+      })
+    ]
+  },
   devServer: {
     port: '3000',
     public: '0.0.0.0',
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,13 @@
 const RollbarSourceMapPlugin = require('rollbar-sourcemap-webpack-plugin')
-const PUBLIC_PATH = 'https://points.live/'
-const webpackConfig = {
-  entry: 'index',
-  publicPath: PUBLIC_PATH,
-  output: {
-    path: 'dist',
-    filename: 'index-[hash].js'
-  },
-  plugins: [new RollbarSourceMapPlugin({
-    accessToken: '992f5159e1344288b41709f4d4910de4',
-    version: '1.0',
-    publicPath: PUBLIC_PATH
-  })]
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new RollbarSourceMapPlugin({
+        accessToken: 'd71e337e9631468e8564e79178e7883f',
+        version: 'version-1',
+        publicPath: 'https://points.live/'
+      })
+    ]
+  }
 }
