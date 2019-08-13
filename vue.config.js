0ad1dd63aabc699f4868e232ff75ef9bd5042329
Riley Shenk <rjshenk@gmail.com>
2019-08-13 16:05:07 -0400

    webpack config not working right now

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,19 @@
-const RollbarSourceMapPlugin = require('rollbar-sourcemap-webpack-plugin')
-const PUBLIC_PATH = 'https://points.live/'
-const path = require('path');
+// const RollbarSourceMapPlugin = require('rollbar-sourcemap-webpack-plugin')
+// const PUBLIC_PATH = 'https://points.live/'
+// const path = require('path');
 
 module.exports = {
-  configureWebpack: {
-    entry: 'index',
-    output: { path: path.resolve(__dirname + '/dist'), filename: '[name].js' },
-    plugins: [new RollbarSourceMapPlugin({
-      accessToken: 'c28199817cb14361b39b960f5b69df86',
-      ignoreErrors: false,
-      publicPath: PUBLIC_PATH,
-      silent: false,
-      version: 'version 1',
-    })],
-  },
+  // configureWebpack: {
+  //   entry: 'index',
+  //   output: { path: path.resolve(__dirname + '/dist'), filename: '[name].js' },
+  //   plugins: [new RollbarSourceMapPlugin({
+  //     accessToken: 'c28199817cb14361b39b960f5b69df86',
+  //     ignoreErrors: false,
+  //     publicPath: PUBLIC_PATH,
+  //     silent: false,
+  //     version: 'version 1',
+  //   })],
+  // },
   devServer: {
     port: '3000',
     public: '0.0.0.0',
