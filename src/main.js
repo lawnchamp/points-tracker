836c251692b1e5cd8e821d54a5a450554c216a22
Riley Shenk <rjshenk@gmail.com>
2019-08-14 00:44:39 -0400

    Changing default vue configs

--- src/main.js
+++ src/main.js
@@ -4,7 +4,23 @@ import router from './router'
 import store from './store'
 const Rollbar = require('vue-rollbar')
 
-Vue.config.productionTip = false
+if (process.env.NODE_ENV == 'production') {
+  Vue.config.productionTip = false
+  Vue.config.devtools = false
+} else {
+  Vue.config.productionTip = true
+  Vue.config.devtools = true
+  Vue.config.performance = true
+}
+Vue.config.silent = false
+Vue.config.errorHandler = function (err, vm, info) {
+  // handle error
+  // `info` is a Vue-specific error info, e.g. which lifecycle hook
+  // the error was found in. Only available in 2.2.0+
+  const stringError = `vue error handler - err:${err}, vm:${vm}, info:${info}`
+  console.error(stringError)
+  Vue.rollbar.error(stringError)
+}
 
 Vue.use(Rollbar, {
   accessToken: '992f5159e1344288b41709f4d4910de4',
